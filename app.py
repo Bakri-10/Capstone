c6c8c1974d660b75b8e061a9b0b4374a397b518e
Abubakri Olayide Abdul-lateef <66844942+Abdul-bakri@users.noreply.github.com>
2022-06-19 13:56:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=80) # specify port=80
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
 
     #host='0.0.0.0', port=80, debug=True
